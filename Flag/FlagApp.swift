6ac8aff8f2973f043fe7c0643cba45152dcbbcca
Varvara Utkina <varya2412@icloud.com>
2025-01-23 13:06:53 +0300

    Initial Commit

---
+++ Flag/FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagApp.swift
+//  Flag
+//
+//  Created by Варвара Уткина on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
